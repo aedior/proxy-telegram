1b57d5d4c9439041218cffba2b6962859b65d718
aliroide <aliroide570@gmail.com>
2024-10-14 10:45:51 -0700

    debug speed proxy

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ MODES = {
 
     # Makes the proxy harder to detect
     # Can be incompatible with very old clients
-    "secure": True,
+    "secure": False,
 
     # Makes the proxy even more hard to detect
     # Can be incompatible with old clients
-    "tls": True
+    "tls": False
 }
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-TLS_DOMAIN = "www.telewebion.com"
+# TLS_DOMAIN = "www.telewebion.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 AD_TAG = "197a18bc6092fa9cda508fbba1a2211d"
